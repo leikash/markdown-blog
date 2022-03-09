eccf1ce93211db05a3b251cde9c8a34efbe2ac77
leikash <yukio.yamada.yy@gmail.com>
2022-03-09 15:07:56 +0800

    Initial commit from gatsby: (https://github.com/leikash/markdown-blog.git)

---
+++ blog-site/src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const BlogPage = () => {
+  return (
+    <div>
+      <Layout>
+      <h1>Leikash Blog</h1>
+      </Layout>
+    </div>
+  )
+}
+
+export default BlogPage
