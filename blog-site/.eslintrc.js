95e7a49ec6ba6b06f120ab80ec74c83e51cacd7c
leikash <yukio.yamada.yy@gmail.com>
2022-03-12 11:29:23 +0800

    remove eslint config

--- blog-site/.eslintrc.js
+++ blog-site/.eslintrc.js
@@ -1,22 +1,34 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  "globals": {
+    __PATH_PREFIX__: true,
+  },
+  "env": {
+    "browser": true,
+    "es2021": true,
+//    "node": true // defines things like process.env when generating through node
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+  ],
+  "parser": "babel-eslint", // uses babel-eslint transforms
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "no-anonymous-exports-page-templates": "warn",
+    "limited-exports-page-templates": "warn",
+  },
+  "settings": {
+    "react": {
+      "version": "detect" // detect react version
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+  },
 };
