96303a8b94e53482a74c5781cd12201a0f94b539
leikash <yukio.yamada.yy@gmail.com>
2022-03-20 18:09:46 +0800

    add new file (not working)

---
+++ blog-site/src/components/blog-list.js
@@ -0,0 +1,57 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image"
+
+/* ファイル分割するときに使う。
+const Bloglist = ({ data }) => {
+    console.log("Bloglist")
+    console.log(data)
+    return (
+        <div>
+          {data.allMarkdownRemark.nodes.map(node => (
+            <div key={node.id}>
+              <Link to={node.fields.slug}>
+                <li>
+                  <h2>
+                    {node.frontmatter.title}
+                  </h2>
+                  <div>
+                    {decidedImageTag(node)}
+                  </div>
+                    {node.frontmatter.date}
+                </li>
+              </Link>
+              <li>
+                {node.frontmatter.summary}
+              </li>
+            </div>
+          ))}
+        </div>
+    )
+  }
+  
+  // 静的イメージ動的イメージのタグを決定して出力する
+  const decidedImageTag = (node) => {
+    const thumbnailImageObj = (node) => (
+      getImage(node.frontmatter.topImage)
+    )
+    if(thumbnailImageObj(node)){
+      return(
+        <GatsbyImage 
+          image={thumbnailImageObj(node)} 
+          alt="${node.frontmatter.title}"
+        />
+      )
+    }else{
+      return(
+        <StaticImage 
+          src="../images/proxyclick-visitor-management-system-nezoFjwiuLQ-unsplash.jpg" 
+          alt="Leikash Blog" 
+          width={300}
+        />
+      )
+    }
+  }
+
+export default Bloglist
+*/
